a11ae4ad698af4e770271e273121ec6d09662a84
Vikesh raut <Vikeshraut952@gmail.com>
2024-06-07 14:06:27 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [value, setValue] = useState(0);
+
+  const addValue = () => {
+    if (value < 20) {
+      setValue(value + 1);
+    } else {
+      alert("No more add possible!");
+    }
+  };
+
+  const removeValue = () => {
+    if (value > 0) {
+      setValue(value - 1);
+    } else {
+      alert("No more remove possible!");
+    }
+  };
+
+  return (
+    <div className="ROOTS">
+      <h1 className="heading">Counter Project</h1>
+      <h2>Counter value : {value}</h2>
+
+      <button onClick={addValue}>Add value : {value}</button>
+      <br />
+
+      <button onClick={removeValue}>Remove value : {value}</button>
+      <br />
+
+      <p>footer : {value}</p>
+    </div>
+  );
+}
+
+export default App;
